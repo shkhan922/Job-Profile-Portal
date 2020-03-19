c4168d2b9f4e8c8dc6707101e4eadcaa49308ce8
shkhan922 <shkhan922@gmail.com>
2020-03-19 20:10:09 +0530

    updated code

--- src/job-sector/components/JobSectorItem.js
+++ src/job-sector/components/JobSectorItem.js
@@ -6,7 +6,7 @@ const JobSectorItem = props => {
         <li className="user-item">
             <div className="user-item__content">
                 <div className="user-item__image">
-                    <img src={props.image} />
+                    <img src={props.image} alt={props.name} />
                 </div>
                 <div className="user-item__info"> 
                   <h2>{props.name}</h2>
