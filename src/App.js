c0f86c73eac6d7f93c4b01c7b1336a7068b395e4
shkhan922 <shkhan922@gmail.com>
2020-03-26 13:46:27 +0530

    new-app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import JobSectors from './job-sector/pages/JobSector';
+import NewProfiles from './job-profile/pages/NewProfile';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
+//import NavLinks from '../src/shared/components/Navigation/NavLinks'
+
+const App = () => {
+  return <Router>
+    <MainNavigation/>
+    <main>
+    <Switch>
+    <Route path="/" exact>
+    <JobSectors/>
+    </Route>
+    <Route path="/jobprofile/new">
+    <NewProfiles/>
+    </Route>
+    <Redirect to='/' />
+    </Switch>
+    </main>
+  </Router>;
 }
 
 export default App;
