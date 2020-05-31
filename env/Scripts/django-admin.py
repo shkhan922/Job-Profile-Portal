4dfc79512213397675c443d4c00289b1a6210768
Shadab <shkhan922@gmail.com>
2020-05-31 16:43:30 +0530

    Updated Code

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\silico\downloads\shadab html (1)\shadab html\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
