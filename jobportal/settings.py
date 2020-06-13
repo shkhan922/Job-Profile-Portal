9341d85839662f375b8850084cd41444bf4e018c
shkhan922 <shkhan922@gmail.com>
2020-06-13 18:48:34 +0530

    Debug False

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k5i$+c^zbm-ivg=(@f(6m*nh_@%xssds(+#q#d=ridefj@to09'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
